3a8d9f899d7c454a3ae6aa07dcd275c39d732635
dennis-199 <69002486+dennis-199@users.noreply.github.com>
2022-07-20 09:16:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import pywhatkit
+#phone_number = input("Enter phone number")
+
+#pywhatkit.sendwhatmsg(phone_number, "Test",8,53)
+pywhatkit.sendwhatmsg_instantly("+254752046968","Hello",15,True,3)
+#pywhatkit.sendwhatmsg(phone_number, "Test",8, 56, 15, True, 2)
+#pywhatkit.sendwhatmsg("+254704754764","Morning Love",9,11,15,True,2)
